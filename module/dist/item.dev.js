f22353d3e1dd460bb0b5935659f4e12785ea8efb
Jupotter <jupotter@jupotter.eu>
2020-10-11 16:55:29 +0200

    Improve item cards

--- module/dist/item.dev.js
+++ module/dist/item.dev.js
@@ -37,34 +37,66 @@ function (_Item) {
   _createClass(EquilibriumItem, [{
     key: "roll",
     value: function roll() {
-      var token, chatData, r;
+      var token, formula, roll, templateData, template, html, chatData;
       return regeneratorRuntime.async(function roll$(_context) {
         while (1) {
           switch (_context.prev = _context.next) {
             case 0:
               token = this.actor.token;
+              formula = this.data.data.damage;
+              roll = null;
+
+              if (formula) {
+                roll = new Roll(this.data.data.damage).roll();
+              }
+
+              ;
+              templateData = {
+                actor: this.actor,
+                item: this.data,
+                data: this.getChatData(),
+                roll: roll,
+                formula: formula
+              };
+              template = "systems/equilibrium/templates/chat/item-card.html";
+              _context.next = 9;
+              return regeneratorRuntime.awrap(renderTemplate(template, templateData));
+
+            case 9:
+              html = _context.sent;
               chatData = {
                 user: game.user._id,
                 type: CONST.CHAT_MESSAGE_TYPES.ROLL,
+                content: html,
                 speaker: {
                   actor: this.actor._id,
                   token: this.actor.token,
                   alias: this.actor.name
                 }
-              }; // // Toggle default roll mode
-              // let rollMode = game.settings.get("core", "rollMode");
-              // if ( ["gmroll", "blindroll"].includes(rollMode) ) chatData["whisper"] = ChatMessage.getWhisperRecipients("GM");
-              // if ( rollMode === "blindroll" ) chatData["blind"] = true;
+              };
+              return _context.abrupt("return", ChatMessage.create(chatData));
 
-              r = new Roll(this.data.data.damage);
-              r.toMessage(chatData);
-
-            case 4:
+            case 12:
             case "end":
               return _context.stop();
           }
         }
       }, null, this);
+    }
+    /**
+       * Prepare an object of chat data used to display a card for the Item in the chat log
+       * @param {Object} htmlOptions    Options used by the TextEditor.enrichHTML function
+       * @return {Object}               An object of chat data to render
+       */
+
+  }, {
+    key: "getChatData",
+    value: function getChatData() {
+      var htmlOptions = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+      var data = duplicate(this.data.data); // Rich text description
+
+      data.description = TextEditor.enrichHTML(data.description, htmlOptions);
+      return data;
     }
   }]);
 
--- module/item.js
+++ module/item.js
@@ -2,21 +2,46 @@ export default class EquilibriumItem extends Item {
     async roll() {
         const token = this.actor.token;
 
+        const formula = this.data.data.damage;
+        let roll = null;
+        if (formula) {
+            roll = new Roll(this.data.data.damage).roll();
+        };
+
+        const templateData = {
+            actor: this.actor,
+            item: this.data,
+            data: this.getChatData(),
+            roll: roll,
+            formula: formula
+        }
+
+        const template = `systems/equilibrium/templates/chat/item-card.html`;
+        const html = await renderTemplate(template, templateData);
         const chatData = {
             user: game.user._id,
             type: CONST.CHAT_MESSAGE_TYPES.ROLL,
+            content: html,
             speaker: {
                 actor: this.actor._id,
                 token: this.actor.token,
                 alias: this.actor.name
-            }
+            },
         }
-        // // Toggle default roll mode
-        // let rollMode = game.settings.get("core", "rollMode");
-        // if ( ["gmroll", "blindroll"].includes(rollMode) ) chatData["whisper"] = ChatMessage.getWhisperRecipients("GM");
-        // if ( rollMode === "blindroll" ) chatData["blind"] = true;
-        
-        let r = new Roll(this.data.data.damage);
-        r.toMessage(chatData)
+
+        return ChatMessage.create(chatData);
     }
+
+/**
+   * Prepare an object of chat data used to display a card for the Item in the chat log
+   * @param {Object} htmlOptions    Options used by the TextEditor.enrichHTML function
+   * @return {Object}               An object of chat data to render
+   */
+  getChatData(htmlOptions={}) {
+    const data = duplicate(this.data.data);
+
+    // Rich text description
+    data.description = TextEditor.enrichHTML(data.description, htmlOptions);
+    return data;
+  }
 }
